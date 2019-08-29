a77da1cec64bca0f48521a3671549434c5167816
tjohnso <tjohnso@odu.edu>
2019-08-28 21:52:27 -0400

    removed thumbnail references from SEO component

--- src/components/common/SEO/index.jsx
+++ src/components/common/SEO/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Thumbnail from 'Static/thumbnail/thumbnail.png'
+
 import {
 	url,
 	defaultDescription,
@@ -45,10 +45,7 @@ export const SEO = ({
 		},
 		"sameAs": [
 			"${socialLinks.twitter}",
-			"${socialLinks.google}",
-			"${socialLinks.youtube}",
 			"${socialLinks.linkedin}",
-			"${socialLinks.instagram}",
 			"${socialLinks.github}"
 		]
   	}`
@@ -56,21 +53,19 @@ export const SEO = ({
 	return (
 		<Helmet>
 			<meta name="description" content={description} />
-			<meta name="image" content={Thumbnail} />
-
-			<meta property="og:url" content={`${url}${location}/?ref=smakosh.com`} />
+			
+			<meta property="og:url" content={`${url}${location}/?ref=tylerjdev.com`} />
 			<meta property="og:type" content="website" />
 			<meta property="og:title" content={title} />
 			<meta property="og:description" content={description} />
-			<meta property="og:image" content={Thumbnail} />
-			<meta property="fb:app_id" content={social.facebook} />
+
 
 			<meta name="twitter:card" content="summary" />
 			<meta name="twitter:creator" content={socialLinks.twitter} />
 			<meta name="twitter:site" content={social.twitter} />
 			<meta name="twitter:title" content={title} />
 			<meta name="twitter:description" content={description} />
-			<meta name="twitter:image:src" content={Thumbnail} />
+			
 			<script type="application/ld+json">{structuredDataOrganization}</script>
 			<link rel="publisher" href={socialLinks.google} />
 			<title>{title}</title>
